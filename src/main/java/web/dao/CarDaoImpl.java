182a93ecb92e52608bb33022dfa1705a222891b8
Romanr1995 <roman.nozdracheff@yandex.ru>
2022-05-05 23:35:27 +0300

    init

---
+++ src/main/java/web/dao/CarDaoImpl.java
@@ -0,0 +1,22 @@
+package web.dao;
+
+import org.springframework.stereotype.Repository;
+import web.model.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class CarDaoImpl implements CarDao {
+
+    private List<Car> cars = List.of(new Car("Model1", 1, 1995),
+            new Car("Model2", 2, 1996),
+            new Car("Model3", 3, 1997),
+            new Car("Model4", 4, 1998),
+            new Car("Model5", 5, 1999));
+
+    @Override
+    public List<Car> getCars(int count) {
+        return cars.stream().limit(count).collect(Collectors.toList());
+    }
+}
